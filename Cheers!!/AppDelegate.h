40fe2e3a22a117174e4eae79a1ba9f6aef2cfcd5
Vinay Raj <vinayraj.dunty@myntra.com>
2014-10-15 11:30:31 +0530

    Initial Commit

---
+++ Cheers!!/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Cheers!!
+//
+//  Created by Vinay Raj Dunty on 15/10/14.
+//  Copyright (c) 2014 Myntra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Cheers!!/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Cheers!!
+//
+//  Created by Vinay Raj Dunty on 15/10/14.
+//  Copyright (c) 2014 Myntra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
